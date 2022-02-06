1275ae44b8d5c0e1f75d1fc4818fce86d858591b
= <=>
2022-02-06 20:47:22 +0700

    add table element 57-71 and 89-103

--- pages/lab3.php
+++ pages/lab3.php
@@ -1,6 +1,10 @@
 <?php
+$data = [];
 $sql = "SELECT * FROM `periordictable`";
 $query = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_array($query)) {
+    array_push($data, $row);
+}
 ?>
 
 <span class="display-4 text-info">Lab 3</span>
@@ -9,12 +13,12 @@ $query = mysqli_query($conn, $sql);
 <hr>
 
 <div class="table-responsive">
-    <table class="table table-bordered">
+    <table class="table table-bordered table-sm">
         <thead class="thead-dark">
             <tr class="text-center">
                 <!-- Generate columns -->
                 <?php for ($i = 1; $i <= 18; $i++) { ?>
-                    <th class="align-middle"><?= $i; ?></th>
+                    <th class="align-middle" scope="col" width="5.55%"><?= $i; ?></th>
                 <?php } ?>
             </tr>
         </thead>
@@ -29,15 +33,11 @@ $query = mysqli_query($conn, $sql);
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
             ];
-            $pos1 = [];
             $count = 0;
             $index_count = 0;
-            for ($i = 1; $i <= 118; $i++) {
-                array_push($pos1, $i);
-            }
             for ($r = 0; $r <= 6; $r++) {
             ?>
-                <tr class="text-center">
+                <tr>
                     <?php for ($i = 1; $i <= 18; $i++) {
                     ?>
                         <td>
@@ -45,9 +45,11 @@ $query = mysqli_query($conn, $sql);
                                 if ($i == $index[$r][$j]) {
                                     $count++;
                                     $index_count++;
-                                    if ($index_count != 57 && $index_count != 75) {
-                                        echo $count;
-                                    }
+                                    if ($index_count != 57 && $index_count != 75) { ?>
+                                        <small class="text-muted"><?= $count; ?></small> <span class="text-danger">(<?= $data[$count - 1]['Symbol']; ?>)</span><br>
+                                        <?= $data[$count - 1]['ElementName']; ?> <br>
+                                        <small><?= $data[$count - 1]['AtomicWeight']; ?> </small><br>
+                            <?php  }
 
                                     if ($index_count == 57) {
                                         $count += 14;
@@ -64,5 +66,39 @@ $query = mysqli_query($conn, $sql);
             <?php } ?>
         </tbody>
 
+    </table>
+</div>
+
+<div class="table-responsive mt-3">
+    <table class="table table-sm table-bordered">
+        <tbody>
+            <tr>
+                <?php for ($i = 54; $i <= 71; $i++) { ?>
+                    <?php if ($i < 57) { ?>
+                        <td width="5.55%"></td>
+                    <?php } else { ?>
+                        <td width="5.55%">
+                        <small class="text-muted"><?= $i; ?></small> <span class="text-danger">(<?= $data[$i - 1]['Symbol']; ?>)</span><br>
+                        <?= $data[$i - 1]['ElementName']; ?> <br>
+                        <small><?= $data[$i - 1]['AtomicWeight']; ?> </small>
+                        </td>
+                    <?php } ?>
+                <?php } ?>
+            </tr>
+
+            <tr>
+                <?php for ($i = 86; $i <= 103; $i++) { ?>
+                    <?php if ($i < 89) { ?>
+                        <td width="5.55%"></td>
+                    <?php } else { ?>
+                        <td width="5.55%">
+                        <small class="text-muted"><?= $i; ?></small> <span class="text-danger">(<?= $data[$i - 1]['Symbol']; ?>)</span><br>
+                        <?= $data[$i - 1]['ElementName']; ?> <br>
+                        <small><?= $data[$i - 1]['AtomicWeight']; ?> </small>
+                        </td>
+                    <?php } ?>
+                <?php } ?>
+            </tr>
+        </tbody>
     </table>
 </div>
