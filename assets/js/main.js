abec6dbfc8661c5d28788b7ee188c3abb9fc9399
onze <slapexs@gmail.com>
2022-02-11 21:37:22 +0700

    add function modalViewElement() for view element from table

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,23 @@ function loadLab(labid) {
   } else {
     $("#super").removeClass("d-none");
     $("#content").addClass("d-none");
-
   }
+}
+
+// Modal view Element
+function modalViewElement(data) {
+  let list = data;
+  $("#modal_name_element").html(list[3]);
+  $("#elem_Pid").html(list[0]);
+  $("#elem_AtomicWeight").html(list[1]);
+  $("#elem_ElementName").html(list[2]);
+  $("#elem_Symbol").html(list[3]);
+  $("#elem_MeltingPoint").html(list[4]);
+  $("#elem_BoilingPoint").html(list[5]);
+  $("#elem_Density").html(list[6]);
+  $("#elem_Discover").html(list[7]);
+  $("#elem_Groups").html(list[8]);
+  $("#elem_ElectronConfiguration").html(list[9]);
+  $("#elem_IonizationEnergy").html(list[10]);
+  $("#modal_viewelement").modal("show");
 }
