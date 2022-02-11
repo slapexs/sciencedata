b5965816035b2387d9553b80e92fe5051eb93a23
onze <slapexs@gmail.com>
2022-02-11 21:39:46 +0700

    New modal for view element from table lab3

---
+++ components/modal_viewelement.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="modal_viewelement" tabindex="-1" aria-labelledby="modal_viewelementLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title text-danger" id="modal_name_element">Modal title</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p><strong>เลขอะตอมธาตุ : </strong><span id="elem_Pid"></span></p>
+                <p><strong>น้ำหนักอะตอม : </strong><span id="elem_AtomicWeight"></span></p>
+                <p><strong>ชื่อเต็มธาตุ : </strong><span id="elem_ElementName"></span></p>
+                <p><strong>ชื่อย่อธาตุ : </strong><span id="elem_Symbol"></span></p>
+                <p><strong>จุดหลอมเหลว : </strong><span id="elem_MeltingPoint"></span></p>
+                <p><strong>จุดเดือด : </strong><span id="elem_BoilingPoint"></span></p>
+                <p><strong>ความหนาแน่น : </strong><span id="elem_Density"></span></p>
+                <p><strong>ปีที่ค้นพบธาตุ : </strong><span id="elem_Discover"></span></p>
+                <p><strong>หมู่ของธาตุ : </strong><span id="elem_Groups"></span></p>
+                <p><strong>การจัดเรียงอิเล็กตรอน : </strong><span id="elem_ElectronConfiguration"></span></p>
+                <p><strong>ค่าพลังงานไอออไนซ์เซชัน : </strong><span id="elem_IonizationEnergy"></span></p>
+            </div>
+        </div>
+    </div>
+</div>
