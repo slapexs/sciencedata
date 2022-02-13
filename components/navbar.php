3ece72be8cd2f4015286aa3f6d713d3dc32d3234
onze <slapexs@gmail.com>
2022-02-13 14:12:27 +0700

    Change title Navbar to Home

--- components/navbar.php
+++ components/navbar.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="./" onclick="loadLab(0)">Lab ทั้งหมด</a>
+        <a class="navbar-brand" href="./" onclick="loadLab(0)">
+            Home
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
