6287a206c885d7df006ef40409776c1083afb2f7
onze <slapexs@gmail.com>
2022-02-18 15:35:28 +0700

    Add background color into elements

--- pages/lab3.php
+++ pages/lab3.php
@@ -7,13 +7,14 @@ while ($row = mysqli_fetch_array($query)) {
 }
 ?>
 
+
 <span class="display-4 text-info">Lab 3</span>
 <h4>โปรแกรมตารางธาตุ</h4>
 
 <hr>
 
 <div class="table-responsive">
-    <table class="table table-bordered table-sm">
+    <table class="table  table-sm">
         <thead class="thead-dark">
             <tr class="text-center">
                 <!-- Generate columns -->
@@ -33,6 +34,16 @@ while ($row = mysqli_fetch_array($query)) {
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
             ];
+
+            $colors = [
+                ['elem_green', 'elem_blue'],
+                ['elem_pink', 'elem_orange', 'elem_darkgreen', 'elem_green', 'elem_green', 'elem_green', 'elem_yellow', 'elem_blue'],
+                ['elem_pink', 'elem_orange', 'elem_grey', 'elem_darkgreen', 'elem_green', 'elem_green', 'elem_yellow', 'elem_blue'],
+                ['elem_pink', 'elem_orange', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_darkgreen', 'elem_darkgreen', 'elem_green', 'elem_yellow', 'elem_blue'],
+                ['elem_pink', 'elem_orange', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_grey', 'elem_darkgreen', 'elem_darkgreen', 'elem_yellow', 'elem_blue'],
+                ['elem_pink', 'elem_orange', 'elem_purple', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_grey', 'elem_grey', 'elem_grey', 'elem_yellow', 'elem_blue'],
+                ['elem_pink', 'elem_orange', 'elem_darkpink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_grey', 'elem_grey', 'elem_grey', 'elem_yellow', 'elem_blue'],
+            ];
             $count = 0;
             $index_count = 0;
             for ($r = 0; $r <= 6; $r++) {
@@ -47,9 +58,9 @@ while ($row = mysqli_fetch_array($query)) {
                                     $index_count++;
                                     //if ($index_count != 57 && $index_count != 75) { 
                             ?>
-                                    <div class="openmodal_viewelement" onclick="modalViewElement(['<?= $data[$count - 1]['Pid']; ?>','<?= $data[$count - 1]['AtomicWeight']; ?>','<?= $data[$count - 1]['ElementName']; ?>','<?= $data[$count - 1]['Symbol']; ?>','<?= $data[$count - 1]['MeltingPoint']; ?>','<?= $data[$count - 1]['BoilingPoint']; ?>','<?= $data[$count - 1]['Density']; ?>','<?= $data[$count - 1]['Discover']; ?>','<?= $data[$count - 1]['Groups']; ?>','<?= $data[$count - 1]['ElectronConfiguration']; ?>','<?= $data[$count - 1]['IonizationEnergy']; ?>'])">
+                                    <div class="openmodal_viewelement <?= $colors[$r][$j] ?>" onclick="modalViewElement(['<?= $data[$count - 1]['Pid']; ?>','<?= $data[$count - 1]['AtomicWeight']; ?>','<?= $data[$count - 1]['ElementName']; ?>','<?= $data[$count - 1]['Symbol']; ?>','<?= $data[$count - 1]['MeltingPoint']; ?>','<?= $data[$count - 1]['BoilingPoint']; ?>','<?= $data[$count - 1]['Density']; ?>','<?= $data[$count - 1]['Discover']; ?>','<?= $data[$count - 1]['Groups']; ?>','<?= $data[$count - 1]['ElectronConfiguration']; ?>','<?= $data[$count - 1]['IonizationEnergy']; ?>'])">
                                         <small class="text-muted float-right"><?= $count; ?></small>
-                                        <h5 class="text-danger mb-0"><?= $data[$count - 1]['Symbol']; ?></h5>
+                                        <h5 class=" mb-0"><?= $data[$count - 1]['Symbol']; ?></h5>
                                         <p class="text-tiny mb-0"><span><?= $data[$count - 1]['ElementName']; ?></span></p>
                                         <p class="text-tiny mb-0"><?= $data[$count - 1]['AtomicWeight']; ?> </p>
                                     </div>
@@ -81,9 +92,9 @@ while ($row = mysqli_fetch_array($query)) {
                     <?php if ($i < 58) { ?>
                         <td width="5.5%"></td>
                     <?php } else { ?>
-                        <td width="5.5%" class="openmodal_viewelement" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
+                        <td width="5.5%" class="openmodal_viewelement elem_purple" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
                             <small class="text-muted float-right"><?= $i; ?></small>
-                            <h5 class="text-danger mb-0"><?= $data[$i - 1]['Symbol']; ?></h5>
+                            <h5 class=" mb-0"><?= $data[$i - 1]['Symbol']; ?></h5>
                             <p class="text-tiny mb-0"><span><?= $data[$i - 1]['ElementName']; ?></span></p>
                             <p class="text-tiny mb-0"><?= $data[$i - 1]['AtomicWeight']; ?> </p>
                         </td>
@@ -96,10 +107,10 @@ while ($row = mysqli_fetch_array($query)) {
                     <?php if ($i < 90) { ?>
                         <td width="5.5%"></td>
                     <?php } else { ?>
-                        <td width="5.5%" class="openmodal_viewelement" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
+                        <td width="5.5%" class="openmodal_viewelement elem_darkpink" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
 
                             <small class="text-muted float-right"><?= $i; ?></small>
-                            <h5 class="text-danger mb-0"><?= $data[$i - 1]['Symbol']; ?></h5>
+                            <h5 class=" mb-0"><?= $data[$i - 1]['Symbol']; ?></h5>
                             <p class="text-tiny mb-0"><span><?= $data[$i - 1]['ElementName']; ?></span></p>
                             <p class="text-tiny mb-0"><?= $data[$i - 1]['AtomicWeight']; ?> </p>
 
