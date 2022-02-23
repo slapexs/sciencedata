80b4a8a0d40083375185ffd6b98b1045fd229974
onze <slapexs@gmail.com>
2022-02-23 14:57:36 +0700

    Change color element metal semi-metal non-metal

--- pages/lab3.php
+++ pages/lab3.php
@@ -35,17 +35,11 @@ while ($row = mysqli_fetch_array($query)) {
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
             ];
 
-            $colors = [
-                ['elem_green', 'elem_blue'],
-                ['elem_pink', 'elem_orange', 'elem_darkgreen', 'elem_green', 'elem_green', 'elem_green', 'elem_yellow', 'elem_blue'],
-                ['elem_pink', 'elem_orange', 'elem_grey', 'elem_darkgreen', 'elem_green', 'elem_green', 'elem_yellow', 'elem_blue'],
-                ['elem_pink', 'elem_orange', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_darkgreen', 'elem_darkgreen', 'elem_green', 'elem_yellow', 'elem_blue'],
-                ['elem_pink', 'elem_orange', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_grey', 'elem_darkgreen', 'elem_darkgreen', 'elem_yellow', 'elem_blue'],
-                ['elem_pink', 'elem_orange', 'elem_purple', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_grey', 'elem_grey', 'elem_grey', 'elem_yellow', 'elem_blue'],
-                ['elem_pink', 'elem_orange', 'elem_darkpink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink', 'elem_litepink','elem_grey', 'elem_grey', 'elem_grey', 'elem_grey', 'elem_yellow', 'elem_blue'],
-            ];
-
+            $metal = [1,2,5,6,7,8,9,10,14,15,16,17,18,32,33,34,35,36,51,52,53,54,86];
+            $semimetal = [5,14,32,33,51,52];
+            $nonmetal = [1,2,6,7,8,9,10,15,16,17,18,34,35,36,53,54,86];
             $rounded_border = [5,14,33,52];
+
             $count = 0;
             $index_count = 0;
             for ($r = 0; $r <= 6; $r++) {
@@ -60,7 +54,7 @@ while ($row = mysqli_fetch_array($query)) {
                                     $index_count++;
                                     //if ($index_count != 57 && $index_count != 75) { 
                             ?>
-                                    <div class="openmodal_viewelement  <?= (in_array($count, $rounded_border) ? 'rounded border_elem' : '')  ?> <?= $colors[$r][$j] ?>" onclick="modalViewElement(['<?= $data[$count - 1]['Pid']; ?>','<?= $data[$count - 1]['AtomicWeight']; ?>','<?= $data[$count - 1]['ElementName']; ?>','<?= $data[$count - 1]['Symbol']; ?>','<?= $data[$count - 1]['MeltingPoint']; ?>','<?= $data[$count - 1]['BoilingPoint']; ?>','<?= $data[$count - 1]['Density']; ?>','<?= $data[$count - 1]['Discover']; ?>','<?= $data[$count - 1]['Groups']; ?>','<?= $data[$count - 1]['ElectronConfiguration']; ?>','<?= $data[$count - 1]['IonizationEnergy']; ?>'])">
+                                    <div class="openmodal_viewelement  <?= (in_array($count, $rounded_border) ? 'rounded border_elem' : '')  ?> <?= (!in_array($count, $metal) ? 'metal' : '') ?> <?= (in_array($count, $semimetal) ? 'semi-metal' : '') ?> <?= (in_array($count, $nonmetal) ? 'non-metal' : '') ?>" onclick="modalViewElement(['<?= $data[$count - 1]['Pid']; ?>','<?= $data[$count - 1]['AtomicWeight']; ?>','<?= $data[$count - 1]['ElementName']; ?>','<?= $data[$count - 1]['Symbol']; ?>','<?= $data[$count - 1]['MeltingPoint']; ?>','<?= $data[$count - 1]['BoilingPoint']; ?>','<?= $data[$count - 1]['Density']; ?>','<?= $data[$count - 1]['Discover']; ?>','<?= $data[$count - 1]['Groups']; ?>','<?= $data[$count - 1]['ElectronConfiguration']; ?>','<?= $data[$count - 1]['IonizationEnergy']; ?>'])">
                                         <small class="text-muted float-right"><?= $count; ?></small>
                                         <h5 class="mb-0"><?= $data[$count - 1]['Symbol']; ?></h5>
                                         <p class="text-tiny mb-0"><span><?= $data[$count - 1]['ElementName']; ?></span></p>
@@ -95,7 +89,7 @@ while ($row = mysqli_fetch_array($query)) {
                         <td width="5.5%"></td>
                     <?php } else { ?>
                         <td width="5.5%">
-                        <div class="openmodal_viewelement  elem_purple" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
+                        <div class="openmodal_viewelement metal" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
                         <small class="text-muted float-right"><?= $i; ?></small>
                             <h5 class="mb-0"><?= $data[$i - 1]['Symbol']; ?></h5>
                             <p class="text-tiny mb-0"><span><?= $data[$i - 1]['ElementName']; ?></span></p>
@@ -113,7 +107,7 @@ while ($row = mysqli_fetch_array($query)) {
                         <td width="5.5%"></td>
                     <?php } else { ?>
                         <td width="5.5%" >
-                        <div class="openmodal_viewelement  elem_darkpink" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
+                        <div class="openmodal_viewelement  metal" onclick="modalViewElement(['<?= $data[$i - 1]['Pid']; ?>','<?= $data[$i - 1]['AtomicWeight']; ?>','<?= $data[$i - 1]['ElementName']; ?>','<?= $data[$i - 1]['Symbol']; ?>','<?= $data[$i - 1]['MeltingPoint']; ?>','<?= $data[$i - 1]['BoilingPoint']; ?>','<?= $data[$i - 1]['Density']; ?>','<?= $data[$i - 1]['Discover']; ?>','<?= $data[$i - 1]['Groups']; ?>','<?= $data[$i - 1]['ElectronConfiguration']; ?>','<?= $data[$i - 1]['IonizationEnergy']; ?>'])">
                         <small class="text-muted float-right"><?= $i; ?></small>
                             <h5 class="mb-0"><?= $data[$i - 1]['Symbol']; ?></h5>
                             <p class="text-tiny mb-0"><span><?= $data[$i - 1]['ElementName']; ?></span></p>
